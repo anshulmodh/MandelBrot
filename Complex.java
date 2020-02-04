8b51984ba5a773a711c67e8a74bc0fb3091a13bb
anshulmodh <35407665+anshulmodh@users.noreply.github.com>
2020-02-04 16:58:51 -0600

    a

--- Complex.java
+++ Complex.java
@@ -1,3 +1,5 @@
+// Complex Number object for mandelBrot set
+
 public class Complex {
     private final double a;
     private final double b;
@@ -7,28 +9,28 @@ public class Complex {
         b = two;
     }
 
-    public Complex add(Complex other) {
+    public Complex add(Complex other) { // Imaginary add
         double newA = a + other.a;
         double newB = b + other.b;
         Complex result = new Complex(newA, newB);
         return result;
     }
 
-    public Complex subtract(Complex other) {
+    public Complex subtract(Complex other) { // Imaginary subtract
         double newA = a - other.a;
         double newB = b - other.b;
         Complex result = new Complex(newA, newB);
         return result;
     }
 
-    public Complex multiply(Complex other) {
+    public Complex multiply(Complex other) { // Imaginary multiply
         double newA = (a * other.a) - (b * other.b);
         double newB = (b * other.a) + (a * other.b);
         Complex result = new Complex(newA, newB);
         return result;
     }
 
-    public Complex divide(Complex other) {
+    public Complex divide(Complex other) { // Imaginary Divide
         Complex reciprocal = new Complex(other.a, other.b * -1);
         Complex one = this.multiply(reciprocal);
         Complex two = other.multiply(reciprocal);
@@ -44,11 +46,11 @@ public class Complex {
         return b;
     }
 
-    public boolean inMandel() {
+    public boolean inMandel() { // check if this number is contained within the mandelbrot set
         return (a <= 2 && b <= 2);
     }
 
-    public Complex square() {
+    public Complex square() { // Square this number and return as a complex number
         Complex other = new Complex(a, b);
         Complex result = this.multiply(other);
         return result;
--- Mandel.java
+++ Mandel.java
@@ -5,20 +5,19 @@ import java.lang.Object;
 import java.awt.image.*;
 import javax.imageio.ImageIO;
 
-public class Mandel extends Thread {
-    int width;
+public class Mandel extends Thread { // MandelBrot object to draw pictures at certain locations in the mandelbrot set
+    int width;          // dimensions of picture
     int height;
-    int h;
-    int v;
-    int squareLen;
-
-    int itr;
-    DrawingPanel panel;
-    int[][] points;
-    double centerX;
-    double centerY;
-    double offset;
-    int draw;
+    int h;              // number of horizontal thread blocks
+    int v;              // number of vertical thread blocks
+    int squareLen;      // Size of each thread square
+    int itr;            // Accuracy of the picture
+    DrawingPanel panel; 
+    int[][] points;     
+    double centerX;     // center x position 
+    double centerY;     // center y position
+    double offset;      // radius of the picture
+    int draw;           // Draw picture while computing (very slow)
     BufferedImage image;
 
     public Mandel(int wi, int hi, int ho, int vo, int square, int i, DrawingPanel p, int[][] pointz, double cx,
@@ -43,16 +42,16 @@ public class Mandel extends Thread {
         double maxX = centerX + offset;
         double minY = centerY - offset;
         double maxY = centerY + offset;
-        double mult = 16777215 / itr;
+        double mult = 16777215 / itr;       // Color multiple for picture RGB(multiple to turn point value into RGB val)
         // BufferedImage = new BuferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        for (int x = h; x < h + squareLen; x++) {
+        for (int x = h; x < h + squareLen; x++) {        // Iterate through every block in the picture
             for (int y = v; y < v + squareLen; y++) {
                 double a = (((maxX - minX) / width) * x) + minX;
                 double b = (((minY - maxY) / height) * y) + maxY;
-                Complex cur = new Complex(a, b);
-                int num = (int) (numItr(cur, itr) * mult);
+                Complex cur = new Complex(a, b);                // assign new number to this point
+                int num = (int) (numItr(cur, itr) * mult);      // calculate whether this point belongs in the set
                 points[y][x] = num;
-                image.setRGB(x, y, num);
+                image.setRGB(x, y, num);                        // Save calculation and color pixel accordingly
                 if (draw == 1){
                     panel.setPixels(points);
                 }
@@ -63,11 +62,11 @@ public class Mandel extends Thread {
         // ".jpg"));
     }
 
-    public static double numItr(Complex num, int itr) {
+    public static double numItr(Complex num, int itr) { // Calculate number of iterations before point diverges from set
         Complex start = new Complex(num.getA(), num.getB());
         Complex cur = new Complex(num.getA(), num.getB());
         double index = 0;
-        while (cur.inMandel()) {
+        while (cur.inMandel()) {  // Iterate from 0 < n < itr iterations or until point diverges
             index++;
             Complex next = cur.square();
             next = next.add(start);
@@ -76,7 +75,7 @@ public class Mandel extends Thread {
                 break;
             }
         }
-        double z = Math.sqrt((cur.getA() * cur.getA()) + (cur.getB() * cur.getB()));
+        double z = Math.sqrt((cur.getA() * cur.getA()) + (cur.getB() * cur.getB())); // Normalize and return
         return index + 1 - (float) Math.log((Math.log(z) / Math.log(2)));
     }
 }
